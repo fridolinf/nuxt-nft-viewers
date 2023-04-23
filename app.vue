9ff3a1ce51187c06548b0f53598ff7532be61986
Your Name <frelly130@gmail.com>
2023-04-24 04:15:58 +0800

    24/04/2023 - add page transition and new package

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtPage />
 </template>
+<script setup lang="ts"></script>
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 2s;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,9 @@ export default defineNuxtConfig({
   typescript: {
     strict: true,
   },
+
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+  router: {},
 });
