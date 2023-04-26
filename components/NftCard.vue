1f0d8e12d91bb057a3d016d5043f97965f0c8d33
Your Name <frelly130@gmail.com>
2023-04-27 00:25:41 +0800

    27/04/2023 - fix static images

--- components/NftCard.vue
+++ components/NftCard.vue
@@ -7,7 +7,7 @@
   >
     <div class="relative">
       <div
-        v-show="nft && nft.image ? nft.image : '/assets/images/noimg.png'"
+        v-show="nft && nft.image ? nft.image : `/assets/images/noimg.png`"
         class="bg-cyan-300 w-32 h-52 absolute md:-right-5 xl:left-2/3 -z-10 rounded-md"
       ></div>
     </div>
@@ -21,7 +21,7 @@
       :src="
         nft && nft.image
           ? nft.image.replace('ipfs://', 'https://cloudflare-ipfs.com/ipfs/')
-          : '../public/assets/images/noimg.png'
+          : `/assets/images/noimg.png`
       "
       :alt="nft && nft.name ? nft.name : 'nft-image'"
     />
